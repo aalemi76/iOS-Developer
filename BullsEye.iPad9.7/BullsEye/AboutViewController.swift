366b243f92984c31df99ad601fe94a67075779f9
aalemi76 <57032234+aalemi76@users.noreply.github.com>
2019-12-29 15:09:13 +0330

    Add files via upload
    Bull's Eye app for ipad 9.7 inch last version before it has replaced by the corresponding app but for your iphone 6 plus
    so it is not completed yet. it takes an hour to be completed at last.

---
+++ BullsEye.iPad9.7/BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by didAR on 12/25/19.
+//  Copyright © 2019 Catalina. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func close (){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
